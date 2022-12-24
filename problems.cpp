a4949cc83574b59eb7bbd2207d8d96fbd50a81ae
Ganesh Shinde <ganeshshinde72620@gmail.com>
2022-12-24 14:24:48 +0530

    update

--- problems.cpp
+++ problems.cpp
@@ -1,41 +1,10 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
-
-class Solution
-{
-public:
-    int TotalPairs(vector<int> &nums, int x, int y)
-    {
-        int pairs = 0;
-        int i = 0;
-        int j = nums.size() - 1;
-        while (i < nums.size())
-        {
-            if (x <= nums[i] * nums[j] && y >= nums[i] * nums[j] && i != j)
-            {
-                pairs++;
-                if (i > j)
-                    i++;
-                else
-                    nums[j] > nums[i] ? j-- : i++;
-                continue;
-            }
-            if (i > j)
-                i++;
-            else
-                nums[j] > nums[i] ? j-- : i++;
-        }
-        return pairs;
-    }
-};
 
 int main()
 {
-    vector<int> a = {3, 5, 5, 2, 6};
-    Solution obj;
-    cout << obj.TotalPairs(a, 8, 13);
-
+    char msg[2][20] = {"ganesh", "shinde"};
+    cout << msg[0] << endl;
+    cout << msg[1] << endl;
     return 0;
 }
