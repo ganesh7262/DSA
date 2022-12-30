f42da91425f28cbb877eb5c62699877ba90775c8
Ganesh Shinde <ganeshshinde72620@gmail.com>
2022-12-30 13:57:29 +0530

    Create stack_adt.cpp

---
+++ stack/stack_adt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/* Based on principle of last in first out */
+/* whenever we insert and remove from the stack the direction of items gets reversed this principle is used in reversing the direction of
+trains in train yards */
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
