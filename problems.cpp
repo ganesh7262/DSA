3c7d17875e6ec72bd60bd0418ecd5192187938b4
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-01-06 22:14:53 +0530

    stack using array

--- problems.cpp
+++ problems.cpp
@@ -1,10 +1,28 @@
+#include <string>
+#include <vector>
 #include <iostream>
+#include <stack>
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    char msg[2][20] = {"ganesh", "shinde"};
-    cout << msg[0] << endl;
-    cout << msg[1] << endl;
+    string s = "ganesh";
+    stack<char> stack;
+    for (int i = 0; i < s.size(); i++)
+    {
+        stack.push(s[i]);
+    }
+
+    char *res = new char[sizeof(s) / sizeof(s[0])];
+    for (int i = 0; i < s.size(); i++)
+    {
+        res[i] = stack.top();
+        stack.pop();
+    }
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        cout << res[i] << endl;
+    }
+
     return 0;
 }
